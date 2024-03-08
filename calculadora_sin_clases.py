03f28597a96ad246e11171916d1b8892444fb46b
Mañanas <markel.berganza@gmail.com>
2024-03-08 09:59:18 +0100

    primer commit calculadora sin clases

---
+++ calculadora_sin_clases.py
@@ -0,0 +1,21 @@
+
+def suma():
+    print("El resultado de la suma es:", a+b)
+    print("El tipo de variable:", type(a+b))
+def resta():
+    print("El resultado de la resta es:", a-b)
+def multip():
+    print("El resultado de la multiplicación es:", c*d)
+    print("El tipo de variable:", type(c*d))
+def divis():
+    print("El resultado de la división es:", a/c)
+    print("El tipo de variable:", type(a/c))
+
+a = 9
+b = 7
+c = 4.5
+d = 8.6
+suma()
+resta()
+multip()
+divis()
