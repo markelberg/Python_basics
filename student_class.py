1c97afb46df0e95350ac387e64467822ee895902
Mañanas <markel.berganza@gmail.com>
2024-03-11 10:58:31 +0100

    another commit

--- student_class.py
+++ student_class.py
@@ -5,6 +5,10 @@ class Student:
 
 std1 = Student("Paco", 32)
 std2 = Student("Ana", 36)
+list = [std1, std2]
 
-print(f"El primer alumno se llama {std1.nombre} y tiene {std1.edad} años")
-print(f"La segunda alumna se llama {std2.nombre} y tiene {std2.edad} años")
+#print(f"El primer alumno se llama {std1.nombre} y tiene {std1.edad} años")
+#print(f"La segunda alumna se llama {std2.nombre} y tiene {std2.edad} años")
+
+for std in list:
+    print(f"Nombre: {std.nombre}, Edad: {std.edad}")
