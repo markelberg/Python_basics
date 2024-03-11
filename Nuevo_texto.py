723c724dd23580e175e615ebc5f86d17e8f7e0e2
Mañanas <markel.berganza@gmail.com>
2024-03-11 10:22:05 +0100

    usando la sentencia with

--- Nuevo_texto.py
+++ Nuevo_texto.py
@@ -4,3 +4,10 @@ archivo.write("Añadiendo texto al archivo desde el editor\n")
 archivo = open("newfile.txt", 'a')
 archivo.write("Adjuntando nuevo texto")
 archivo.close()
+
+#Es más óptimo y "limpio" si lo hacemos con with ('r' para read, 'w' para write, 'a' para append)
+
+with open("newfile.txt", 'w') as archivo:
+    lectura = archivo.write("Sobreescribiendo el txt con with")
+    print(lectura)
+
